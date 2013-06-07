c65fcf7fa4970b20956a3661471f6cb96cc06413
David Nguyen <diepnn@gmail.com>
2013-06-08 00:01:27 +1000

    Version 1.2.0 added demo page and fixes

--- bootstrap.validate.js
+++ bootstrap.validate.js
@@ -2,7 +2,8 @@
  * Bootstrap Validate Scriptlet that supports Twitter Bootstrap styling to 
  * jQuery validate and unobstrusive jQuery validate
  * Author: David Nguyen (diepnn@gmail.com)
- * Version 1.1.0
+ * History:
+ * - v1.2.0: added a fix from mgreuel to only triggers validation on keyup when the field is already in error state
  */
 
 (function ($) {
@@ -41,21 +42,16 @@
     },
     onFormInputChange = function () {
         /// <summary>Handles form input change event</summary>
-        var parentForm = $(this).parents(elements.form);
-        if ($(this).valid()) {
-            parentForm.find(elements.controlGroupDiv).each(function () {
-                if ($(this).find(elements.inputValidationError).length == 0) {
-                    $(this).removeClass(classes.error);
-                }
-            });
-        }
-        else {
-            parentForm.find(elements.controlGroupDiv).each(function () {
-                if ($(this).find(elements.inputValidationError).length > 0) {
-                    $(this).addClass(classes.error);
-                }
-            });
-        }
+        var $controlGroup = $(this).parents(elements.controlGroupDiv);
+        
+        // The delay leaves jquery validate some time to insert the validation error
+        setTimeout(function() {
+            if ($controlGroup.find(elements.inputValidationError).length == 0) {
+                $controlGroup.removeClass(classes.error);
+            } else {
+                $controlGroup.addClass(classes.error);
+            }
+        }, 500);
     },
     highlightInitialisedErrors = function () {
         /// <summary>Highlight error inputs that have been initialised (e.g. part of initial page load)</summary>
@@ -72,6 +68,7 @@
             $(elements.form).off('submit', onFormSubmit).on('submit', onFormSubmit);
             $(elements.formInput).off('change', onFormInputChange).on('change', onFormInputChange);
             $(elements.formInput).off('keyup', onFormInputChange).on('keyup', onFormInputChange);
+            $(elements.formInput).off('blur', onFormInputChange).on('blur', onFormInputChange);
             $(elements.form).each(highlightInitialisedErrors);
         });
     };
